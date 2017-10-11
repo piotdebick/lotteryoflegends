e59dbe986e7a268bbb62e73b974c629a968c6f03
Piotr <piotr.debicki@cix.csi.cuny.edu>
2017-10-11 17:45:59 -0400

    changed some http calls to https

--- src/components/champion-components/Champion.js
+++ src/components/champion-components/Champion.js
@@ -4,7 +4,7 @@ const Champion = props => (
     <img
         className='list-item__display'
         type="image"
-        src={`http://ddragon.leagueoflegends.com/cdn/7.17.1/img/champion/${props.name}.png`}
+        src={`https://ddragon.leagueoflegends.com/cdn/7.17.1/img/champion/${props.name}.png`}
       />
 );
 
